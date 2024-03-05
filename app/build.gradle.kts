043464b2d3b58ade2734bf27ea34d3e0435ffb4c
Pavel <p.belonogov@hotmail.com>
2024-03-05 12:13:54 +0500

    Добавил // Разрешение на доступ к интернету в манифест и зависимости в Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,12 @@ android {
 
 dependencies {
 
+    //Network calls
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    //Json to Kotlin object mapping
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
